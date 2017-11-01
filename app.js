f81849e79af4c82ac6dea6d527667ebd3a91b5b5
PrashanthVasa <prashanthvasa55@gmail.com>
2017-11-01 19:21:11 +0530

    modularised the code to geocode folder

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const request = require('request');
 const yargs = require('yargs');
+const geocode = require('./geocode/geocode');
 
 const argv = yargs
     .options({
@@ -14,17 +14,12 @@ const argv = yargs
     .alias('help','h')
     .argv;
 
-console.log(argv.address);
-encodedAddress = encodeURIComponent(argv.address);
-//console.log(encodedAddress); 
-
-request({
-    url:'https://maps.googleapis.com/maps/api/geocode/json?address='+encodedAddress,
-    json:true
-},(error,response,body)=>{
-    //console.log(JSON.stringify(error,undefined,2));
-    console.log('Address: '+body.results[0].formatted_address);
-    console.log('Lattitude: '+body.results[0].geometry.location.lat);
-    console.log('Longitude: '+body.results[0].geometry.location.lng);
-    
+geocode.geocodeAddress(argv.address,(errorMessage,results)=>{
+    if(errorMessage){
+        console.log(errorMessage);
+    }
+    else{
+        console.log(JSON.stringify(results,undefined,2));
+    }
 });
+
